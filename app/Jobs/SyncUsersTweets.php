7d2442d60f0bfe7604e08f50b4a1c4fcbe064344
Ziad <zeiad.suliman@gmail.com>
2019-03-11 00:59:52 +0200

    - Add Tweet formatter - Add TweetsSync Handler

---
+++ app/Jobs/SyncUsersTweets.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Contracts\TwitterHandlerContract;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SyncUsersTweets implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        /** @var TwitterHandlerContract $twitterHandler */
+        $twitterHandler = app(TwitterHandlerContract::class);
+        $twitterHandler->syncTweets();
+
+    }
+}
