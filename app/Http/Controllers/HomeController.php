76f946acceabb4d47248de0946af588b1290a577
Ziad <zeiad.suliman@gmail.com>
2019-03-11 01:08:55 +0200

    fix welcome route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('welcome');
     }
 
     /**
@@ -24,5 +24,10 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function welcome()
+    {
+        return view('welcome');
     }
 }
