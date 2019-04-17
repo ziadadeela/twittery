9470b07febe576e4938bfa8cf8dd62f54209a7ec
Ziad Adeela <ziad@souktel.org>
2019-04-17 11:10:09 +0300

    separate menu logic in a separate blade. Fix APP name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,37 +56,7 @@
 <div class="container-fluid">
     <div class="row">
         <!-- Main Sidebar -->
-        <aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
-            <div class="main-navbar">
-                <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-                    <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
-                        <div class="d-table m-auto">
-                            <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;"
-                                 src="{{asset('images/shards-dashboards-logo.svg')}}"
-                                 alt="{{ config('app.name', 'Twittery') }}">
-                            <span class="d-none d-md-inline ml-1">{{ config('app.name', 'Twittery') }}</span>
-                        </div>
-                    </a>
-                    <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
-                        <i class="material-icons">&#xE5C4;</i>
-                    </a>
-                </nav>
-            </div>
-            <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
-                <div class="input-group input-group-seamless ml-3">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">
-                            <i class="fas fa-search"></i>
-                        </div>
-                    </div>
-                    <input class="navbar-search form-control" type="text" placeholder="Search for something..."
-                           aria-label="Search">
-                </div>
-            </form>
-            <div class="nav-wrapper">
-                {!! Menu::sideBar() !!}
-            </div>
-        </aside>
+        @include('partials.sidebar')
         <!-- End Main Sidebar -->
         <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
 
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,22 +1,31 @@
-<!-- BEGIN SIDEBAR -->
-<div class="sidebar">
-    <div class="logopanel">
-        <h1>
-            <a href="{{ url('dashboard') }}"></a>
-        </h1>
+<aside class="main-sidebar col-12 col-md-3 col-lg-2 px-0">
+    <div class="main-navbar">
+        <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
+            <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
+                <div class="d-table m-auto">
+                    <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;"
+                         src="{{asset('images/shards-dashboards-logo.svg')}}"
+                         alt="{{ config('app.name', 'Twittery') }}">
+                    <span class="d-none d-md-inline ml-1">{{ config('app.name', 'Twittery') }}</span>
+                </div>
+            </a>
+            <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
+                <i class="material-icons">&#xE5C4;</i>
+            </a>
+        </nav>
     </div>
-    <div class="sidebar-inner">
-        <div class="menu-title">
-            Welcome <strong>{{ user()->username }}</strong>
+    <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
+        <div class="input-group input-group-seamless ml-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+            <input class="navbar-search form-control" type="text" placeholder="Search for something..."
+                   aria-label="Search">
         </div>
-        <ul class="nav nav-sidebar">
-            <li class="{{ \Request::is('dashboard*')?'nav-active active':'' }}">
-                <a href="{{ url('dashboard') }}">
-                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                </a>
-            </li>
-            @include('partials.menu.menu_item', ['menus'=>\Menus::getMenu('sidebar','active')])
-        </ul>
+    </form>
+    <div class="nav-wrapper">
+        {!! Menu::sideBar() !!}
     </div>
-</div>
-<!-- END SIDEBAR -->
+</aside>
